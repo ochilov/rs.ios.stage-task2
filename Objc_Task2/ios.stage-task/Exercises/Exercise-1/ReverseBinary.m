da66f166540377c016ee2e84a66cb83e3dbe2b0c
JohnO <>
2021-05-14 18:37:30 +0300

    feat: Exercise-1: implement ReverseInteger function

--- Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-1/ReverseBinary.m
@@ -1,5 +1,11 @@
 #import "ReverseBinary.h"
 
 UInt8 ReverseInteger(UInt8 n) {
-    return 0;
+	UInt8 reversed = 0;
+	for (UInt8 i = 0, bitsCount = 8; i < bitsCount; i++) {
+		if (n & (1 << i)) {
+			reversed |= 1 << (bitsCount-i-1);
+		}
+	}
+	return reversed;
 }
