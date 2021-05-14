5b40e01394b80455119d52586667917083b4a181
JohnO <>
2021-05-14 18:55:02 +0300

    feat: Exercise-2: implement countPairs function

--- Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-2/Pairs.m
@@ -3,7 +3,18 @@
 @implementation Pairs
 
 - (NSInteger)countPairs:(NSArray <NSNumber *> *)array number:(NSNumber *)number {
-    return 0;
+	NSInteger differsCount = 0;
+	for (NSUInteger i = 0; i < array.count-1; i++) {
+		NSNumber *a = [array objectAtIndex:i];
+		for (NSUInteger j = i+1; j < array.count; j++) {
+			NSNumber *b = [array objectAtIndex:j];
+			// TODO: if numbers in array is not sorted - it is necessary to use abs in comparison
+			if (b.integerValue - a.integerValue == number.integerValue) {
+				differsCount++;
+			}
+		}
+	}
+    return differsCount;
 }
 
 @end
