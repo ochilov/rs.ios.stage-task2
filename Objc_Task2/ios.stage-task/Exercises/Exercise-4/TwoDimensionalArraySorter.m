15ffeccb19238553b5d93fdbba46985c56b64036
JohnO <>
2021-05-14 23:20:47 +0300

    feat: Exercise-4: implement twoDimensionalSort function

--- Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
+++ Objc_Task2/ios.stage-task/Exercises/Exercise-4/TwoDimensionalArraySorter.m
@@ -3,7 +3,31 @@
 @implementation TwoDimensionalArraySorter
 
 - (NSArray *)twoDimensionalSort:(NSArray<NSArray *> *)array {
-    return @[];
+	NSMutableArray *numbers = [[NSMutableArray alloc] init];
+	NSMutableArray *strings = [[NSMutableArray alloc] init];
+	for (id item in array) {
+		if (![item isKindOfClass:NSArray.class]) {
+			return @[];
+		}
+		NSArray *subArray = (NSArray *)item;
+		if (!subArray.count) {
+			continue;
+		}
+		if ([subArray.firstObject isKindOfClass:NSNumber.class]) {
+			[numbers addObjectsFromArray:subArray];
+		} else if ([subArray.firstObject isKindOfClass:NSString.class]) {
+			[strings addObjectsFromArray:subArray];
+		}
+	}
+	[numbers sortUsingSelector:@selector(compare:)];
+	[strings sortUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
+	if (strings.count && numbers.count) {
+		return @[ [numbers copy], [strings copy] ];
+	}
+	if (numbers.count) {
+		return [numbers copy];
+	}
+	return [strings copy];
 }
 
 @end
